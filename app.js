db115a0c315f7bf8d23ae1f34a711b222fc51e64
kingoftheconnors <Connormcp777@gmail.com>
2020-05-14 11:58:06 -0400

    Output graph to webpage

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var express = require("express");
 const puppeteer = require("puppeteer");
 var app = express();
-const exportGraph = require("./lib/chart");
+//const exportGraph = require("./lib/chart");
 const processPage = require("./lib/process");
 
 async function run() {
   //const browser = await puppeteer.launch();
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   page.on('console', msg => console.log(msg.text()));
   
-
   console.log("The Process has started....");
   await page.setViewport({ width: 1920, height: 1080 });
 
@@ -20,19 +19,14 @@ async function run() {
   }
   );
 
-  await page.screenshot({ path: './public/images/1.jpg', type: 'jpeg', fullPage: true });
-
-  //   await Promise.all([
-  //     page.waitForNavigation(),
-  //     console.log("All shit has been done now")
-  // ]);
+  //await page.screenshot({ path: './public/images/1.jpg', type: 'jpeg', fullPage: true });
 
   await Promise.all([
     // page.waitForNavigation(waitOptions),
     page.click("#classSearchLink"),
   ]);
 
-  await page.screenshot({ path: './public/images/2.jpg', type: 'jpeg', fullPage: true });
+  //await page.screenshot({ path: './public/images/2.jpg', type: 'jpeg', fullPage: true });
 
   await page.evaluate(() => {
     // let elements = document.getElementById("txt_term")
@@ -45,17 +39,14 @@ async function run() {
     page.waitForNavigation({ waitUntil: 'networkidle2' }),
   ]);
 
-  await page.screenshot({ path: './public/images/3.jpg', type: 'jpeg', fullPage: true });
+  //await page.screenshot({ path: './public/images/3.jpg', type: 'jpeg', fullPage: true });
 
   await page.evaluate(() => {
-    // let elements = document.getElementById("txt_term")
     return document.getElementById("txt_subject").setAttribute("value", "CPSC"); // CPSC,SUBJ,ANTH,etc to get multiple subjects
   });
 
   await Promise.all([
-    // page.waitForNavigation(waitOptions),
     page.click("#search-go"),
-    //page.waitForNavigation({ waitUntil: 'networkidle2' }),
     page.waitForSelector(".meeting")
   ]);
 
@@ -89,27 +80,25 @@ async function run() {
     ]);
   }
 
-  exportGraph(calendarData)
-
-
-  await Promise.all([
-
-    page.screenshot({ path: './public/images/4.jpg', type: 'jpeg', fullPage: true }),
-    page.waitForNavigation({ waitUntil: 'networkidle2' }),
-  ]);
-
-  console.log("Server has started on port 8080");
-
   await page.close();
   await browser.close();
+
+  return calendarData
 }
 
-run()
-/*
-app.use("/home", (req, res) => {
-  res.send("Worked")
-  run();
-})*/
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.static(__dirname + '/public'))
 
+app.get('/chart', function(req,res) {
+  run().then(chartData => {
+    console.log(chartData)
+    res.render('chart',
+      { name: "Class Layout by Week",
+        barArray: chartData,
+      });
+  });
+});
 
+console.log("Server has started on port 8080");
 app.listen(8080)
