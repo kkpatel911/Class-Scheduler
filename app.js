1124404de65ec88ea08a918f7433bda8fbe04c23
kingoftheconnors <Connormcp777@gmail.com>
2020-10-10 00:05:46 -0400

    Extract database calls into data tier
    For the sake of encapsulation, the database calls should be separate
    from the controller. This way, the database backend can be substituted
    if the service has to change cloud providers.

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
+// Load packages
 var express = require("express");
 const bodyParser = require('body-parser');
 var app = express();
 const fs = require("fs");
 const buildCalendar = require("./lib/buildCalendar");
 const readClassInfo = require("./lib/readClass");
-const { create } = require("domain");
+const dataTier = require("./lib/dataTier");
 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
+// Set up express
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Website Root
 app.get('/', function(req,res) {
     res.render('configSearch');
 });
 
+// Show/Load calendar
 app.get('/chart', function(req,res) {
   // TODO: Translate data from configSearch front-end
   /*console.log(req.body.major1)
   console.log(req.body.classNumber1)
   console.log(req.body.geneds)*/
+  // TODO: If an extension is noted, call it by id
 
   // Make front-end somehow allow users to submit all classes as a single array:
   var mockInput = ["CPSC1100", "CPSC1110", "CPSC2100", "MATH1950", "MATH1960"]
@@ -55,7 +49,7 @@ app.get('/chart', function(req,res) {
       });
 });
 
-// Ajax call. Returns status of save
+// Save calendar
 app.post('/saveChart', function(req,res) {
   // TODO: Get data from request
   var mockInput = {
@@ -90,27 +84,12 @@ app.post('/saveChart', function(req,res) {
   var mockName = "NewCalendar"
 
   // Save data in database
-  client.query('INSERT INTO calendar(name) VALUES ($1) RETURNING calendarID;', [mockName], (err, res) => {
-    if (err) throw err;
-    console.log("CalendarID: ", res.rows[0]['calendarid'])
-    var calendarID = res.rows[0]['calendarid'];
-    if (calendarID > 0) {
-      for (var className in mockInput) {
-        client.query('INSERT INTO calendarRow(calendarid, classname, timeslots) VALUES ($1, $2, $3);', [calendarID, className, mockInput[className]]);
-      }
-    }
-  });
-  /*client.query('SELECT * FROM Calendar;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });*/
+  dataTier.saveCalendar(mockInput, mockName)
+
   res.render('configSearch');
 });
 
-// Create a search page to find calendars by their name
+// TODO: Create a search page to find calendars by their name
 
 
 console.log("Server has started on port 8080");
