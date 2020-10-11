7a7f3d251c299e99fd6417926530ea8d5e574f2c
kingoftheconnors <Connormcp777@gmail.com>
2020-10-11 10:31:05 -0400

    Saving calendar

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ app.get('/chart', function(req,res) {
         
         // Numbers 1-27 represent 8:00am-9:30pm on Monday by half-hour. 28-54 represent 8:00am-9:30pm on Tuesday.
         var createdCalendar = buildCalendar(mockInput, pertinentClassData); // Created calendar format: { CPSC1100: [0, 1, 2, 8, 9, 12, 13, 41, 42], CPSC1110: [22, 76], ... }
-        console.log(createdCalendar)
         
         res.render('chart',
           { name: "Class Layout by Week",
@@ -52,41 +51,19 @@ app.get('/chart', function(req,res) {
 // Save calendar
 app.post('/saveChart', function(req,res) {
   // TODO: Get data from request
-  var mockInput = {
-    CPSC1100: [
-       62, 63, 64,  65,  66,  67, 68,   2,   3,  56, 57, 110,
-      111, 44, 45,  46,  47,  67, 68,  69,  70,  41, 42,  43,
-       95, 96, 97,   4,   5,  58, 59, 112, 113,  13, 14,  15,
-       16, 22, 23,  24,  25,  26, 76,  77,  78,  79, 80,   6,
-        7, 60, 61, 114, 115,  44, 45,  46,  47,  62, 63,  64,
-       65, 66, 67,  68,   2,   3, 56,  57, 110, 111, 44,  45,
-       46, 47, 67,  68,  69,  70, 41,  42,  43,  95, 96,  97,
-        4,  5, 58,  59, 112, 113, 13,  14,  15,  16, 22,  23,
-       24, 25, 26,  76
-    ],
-    CPSC1110: [
-       6,  7, 60, 61, 114, 115, 42, 43, 44, 45,  31,  32,
-      33, 34, 15, 16,  17,  69, 70, 71, 31, 32,  33,  34,
-       6,  7, 60, 61, 114, 115,  6,  7, 60, 61, 114, 115,
-      42, 43, 44, 45,  31,  32, 33, 34, 15, 16,  17,  69,
-      70, 71, 31, 32,  33,  34,  6,  7, 60, 61, 114, 115,
-       6,  7, 60, 61, 114, 115, 42, 43, 44, 45,  31,  32,
-      33, 34, 15, 16,  17,  69, 70, 71, 31, 32,  33,  34,
-       6,  7, 60, 61, 114, 115
-    ],
-    MATH1950: [
-      0, 1, 2, 54, 55, 56, 108, 109, 110,
-      6, 7, 8, 60, 61, 62, 114, 115, 116,
-      0, 1, 2, 54, 55, 56, 108, 109, 110,
-      6, 7, 8, 60, 61, 62, 114, 115, 116
-    ]
-  }
-  var mockName = "NewCalendar"
+  var calendarInput = JSON.parse(req.body.calendarData);
+  var calendarName = req.body.calendarName;
 
   // Save data in database
-  dataTier.saveCalendar(mockInput, mockName)
+  dataTier.saveCalendar(calendarInput, calendarName);
 
   res.render('configSearch');
+  res.render('chart',
+    { name: calendarName,
+      barArray: JSON.stringify(calendarInput),
+      chartX: 5,
+      chartY: 27
+    });
 });
 
 // TODO: Create a search page to find calendars by their name
