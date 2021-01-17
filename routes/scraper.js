094ccc385f0611b383c4ff1406541c40e8041512
kingoftheconnors <Connormcp777@gmail.com>
2021-01-17 12:53:05 -0500

    Update scraper endpoint to use new scraper functionality

--- routes/scraper.js
+++ routes/scraper.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const dataTier = require("../lib/dataTier");
 const scraper = require("../lib/scrapper");
+const utils = require("../lib/utils");
 
 // POST calendar to database
 router.get('/', function(req,res) {
@@ -9,7 +10,8 @@ router.get('/', function(req,res) {
 
   // Clear class tables
   dataTier.clearClassData()
-  scraper.scrapeData()
+  // Double-check for subjects that don't have any classes
+  scraper.scrapeUnusedClasses(utils.getTerm())
 });
 
 module.exports = router;
